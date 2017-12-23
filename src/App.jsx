b6df8e9ab7b8f61c787a4e8c48f57d0b34ec4e74
Albert Blanco-Cuaresma <bertobc@users.noreply.github.com>
2017-12-23 02:39:21 +0100

    + validations

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,22 @@ import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
+import Message from './components/Message';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             users: [],
-            username: '',
-            email: '',
-            title: 'ORbit.io',
-            formData: {
-                username: '',
-                email: '',
-                password: ''
-            },
-			isAuthenticated: false
+            title: 'TestDriven.io',
+            isAuthenticated: false,
+            messageName: null,
+            messageType: null
         }
     }
     componentWillMount() {
         if (window.localStorage.getItem('authToken')) {
-            this.setState({ isAuthenticated: true });
+            this.setState({ isAuthenticated: true  });
         }
     }
     componentDidMount() {
@@ -38,116 +34,88 @@ class App extends Component {
             .then((res) => { this.setState({ users: res.data.data.users }); })
             .catch((err) => { console.log(err); })
     }
-    addUser(event) {
-        event.preventDefault();
-        const data = {
-            username: this.state.username,
-            email: this.state.email
-        }
-        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-            .then((res) => {
-                this.getUsers();
-                this.setState({ username: '', email: '' });
-            })
-            .catch((err) => { console.log(err); })
-    }
     logoutUser() {
         window.localStorage.clear();
-        this.setState({ isAuthenticated: false  });
+        this.setState({ isAuthenticated: false });
     }
-    handleChange(event) {
-        const obj = {};
-        obj[event.target.name] = event.target.value;
-        this.setState(obj);
+    loginUser(token) {
+        window.localStorage.setItem('authToken', token);
+        this.setState({ isAuthenticated: true });
+        this.getUsers();
+        this.createMessage('Welcome!', 'success');
     }
-	handleUserFormSubmit(event) {
-		event.preventDefault();
-		const formType = window.location.href.split('/').reverse()[0];
-		let data;
-		if (formType === 'login') {
-			data = {
-				email: this.state.formData.email,
-				password: this.state.formData.password
-			}
-		}
-		if (formType === 'register') {
-			data = {
-				username: this.state.formData.username,
-				email: this.state.formData.email,
-				password: this.state.formData.password
-			}
-		}
-		const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
-		axios.post(url, data)
-			.then((res) => {
-				this.setState({
-					formData: {username: '', email: '', password: '' },
-					username: '',
-					email: '',
-					isAuthenticated: true
-				});
-				window.localStorage.setItem('authToken', res.data.auth_token);
-                this.getUsers();
-			})
-			.catch((err) => { console.log(err); })
-	}
-	handleFormChange(event) {
-		const obj = this.state.formData;
-		obj[event.target.name] = event.target.value;
-		this.setState(obj);
+    createMessage(name='Sanity Check', type='success') {
+        this.setState({
+            messageName: name,
+            messageType: type
+        })
+		setTimeout(() => {
+			this.removeMessage()
+		}, 3000);
+    }
+	removeMessage() {
+		this.setState({
+			messageName: null,
+			messageType: null
+		})
 	}
     render() {
         return (
-		<div>
-		  <NavBar
-			title={this.state.title}
-			isAuthenticated={this.state.isAuthenticated}
-              />
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6">
-                    <br/>
-                    <Switch>
-					<Route exact path='/' render={() => (
-						<UsersList
-					  	  users={this.state.users}
-					    />
-				  	)} />
-                    <Route exact path='/about' component={About}/>
-					<Route exact path='/register' render={() => (
-					  <Form
-						formType={'Register'}
-						formData={this.state.formData}
-						handleUserFormSubmit={this.handleUserFormSubmit.bind(this)}
-						handleFormChange={this.handleFormChange.bind(this)}
-                        isAuthenticated={this.state.isAuthenticated}
-					  />
-					)} />
-					<Route exact path='/login' render={() => (
-					  <Form
-						formType={'Login'}
-						formData={this.state.formData}
-						handleUserFormSubmit={this.handleUserFormSubmit.bind(this)}
-						handleFormChange={this.handleFormChange.bind(this)}
-                        isAuthenticated={this.state.isAuthenticated}
-					  />
-					)} />
-                    <Route exact path='/logout' render={() => (
-                          <Logout
-                            logoutUser={this.logoutUser.bind(this)}
-                            isAuthenticated={this.state.isAuthenticated}
-                          />
+          <div>
+            <NavBar
+              title={this.state.title}
+              isAuthenticated={this.state.isAuthenticated}
+            />
+            <div className="container">
+              {this.state.messageName && this.state.messageType &&
+                  <Message
+                    messageName={this.state.messageName}
+                    messageType={this.state.messageType}
+					removeMessage={this.removeMessage.bind(this)}
+                  />
+                }
+              <div className="row">
+                <div className="col-md-6">
+                  <br/>
+                  <Switch>
+                    <Route exact path='/' render={() => (
+                      <UsersList
+                        users={this.state.users}
+                      />
                     )} />
-					<Route exact path='/status' render={() => (
-					  <UserStatus
-						isAuthenticated={this.state.isAuthenticated}
-					  />
-					)} />
-                    </Switch>
-                  </div>
+                    <Route exact path='/about' component={About}/>
+                    <Route exact path='/register' render={() => (
+                      <Form
+                        formType={'register'}
+                        isAuthenticated={this.state.isAuthenticated}
+                        loginUser={this.loginUser.bind(this)}
+						createMessage={this.createMessage.bind(this)}
+                      />
+                    )} />
+                    <Route exact path='/login' render={() => (
+                      <Form
+                        formType={'login'}
+                        isAuthenticated={this.state.isAuthenticated}
+                        loginUser={this.loginUser.bind(this)}
+						createMessage={this.createMessage.bind(this)}
+                      />
+                    )} />
+                    <Route exact path='/logout' render={() => (
+                      <Logout
+                        logoutUser={this.logoutUser.bind(this)}
+                        isAuthenticated={this.state.isAuthenticated}
+                      />
+                    )} />
+                    <Route exact path='/status' render={() => (
+                      <UserStatus
+                        isAuthenticated={this.state.isAuthenticated}
+                      />
+                    )} />
+                  </Switch>
                 </div>
               </div>
             </div>
+          </div>
         )
     }
 }
